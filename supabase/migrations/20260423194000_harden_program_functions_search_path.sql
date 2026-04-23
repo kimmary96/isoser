9a1b21732d9433579c92251f098e081853152e78
kimmary96 <kimgone125@gmail.com>
2026-04-23 18:10:25 +0900

    fix: remove absent category detail function hardening

--- supabase/migrations/20260423194000_harden_program_functions_search_path.sql
+++ supabase/migrations/20260423194000_harden_program_functions_search_path.sql
@@ -1,7 +1,6 @@
 alter function public.update_programs_search_text() set search_path = public;
 alter function public.programs_searchable_compare_meta_text(jsonb) set search_path = public;
 alter function public.infer_program_category_detail(text, text, text, text, text[], text[], jsonb) set search_path = public;
-alter function public.update_programs_category_detail() set search_path = public;
 alter function public.set_updated_at() set search_path = public;
 alter function public.program_list_try_date(text) set search_path = public;
 alter function public.program_list_text_array(jsonb) set search_path = public;
